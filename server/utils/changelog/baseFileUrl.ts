8189a99e523d53ee91eae44b7018d39b78da999f
WilcoSp <wilcosp@hotmail.nl>
2026-08-05 23:17:37 +0200

    quick fix for codeberg

--- server/utils/changelog/baseFileUrl.ts
+++ server/utils/changelog/baseFileUrl.ts
@@ -11,15 +11,19 @@ export function getBaseFileUrl(ref: RepoRef): RepoFileUrl | null {
         blob: `https://github.com/${ref.owner}/${ref.repo}/blob/HEAD`,
       }
     case 'codeberg':
+      return {
+        blob: `https://codeberg.org/${ref.owner}/${ref.repo}/src/branch/HEAD`,
+        raw: `https://codeberg.org/${ref.owner}/${ref.repo}/raw/branch/HEAD`,
+      }
     case 'forgejo': {
-      const host = ref.host ?? 'codeberg.org'
+      const host = ref.host
       return {
         blob: `https://${host}/${ref.owner}/${ref.repo}/src/branch/HEAD`,
         raw: `https://${host}/${ref.owner}/${ref.repo}/raw/branch/HEAD`,
       }
     }
     case 'gitlab': {
-      const host = ref.host ?? 'gitlab.com'
+      const host = ref.host || 'gitlab.com'
       return {
         blob: `https://${host}/${ref.owner}/${ref.repo}/-/blob/HEAD`,
         raw: `https://${host}/${ref.owner}/${ref.repo}/-/raw/HEAD`,
